ec9c250abc6adf4dfb4de85ea50c998e7b626d4d
Farous Arthur <arthurfarous6@gmail.com>
2023-05-26 08:50:34 +0000

    js assignment

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,23 @@
+
+// declaraction of variable(myName)
+let myName = "Farous";
+
+// how to check for the data type of my variable(myName)
+console.log(typeof myName)
+
+// changing of variable(myName) to my surname
+myName = "Arthur"
+
+// declaraction of an object(car) with properties
+let car = { 
+  ownerName: "Farous Arthur",
+  make:  "Honda",
+  model: "CR-V",
+  color: "white",
+  yearOf_manufacture:2023,
+  mileage : "14kmpl",
+  Price : 4000000,
+  condition : "Foreign Used",
+  transmission: "Automatic",
+   Registered:  false,
+}
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,21 @@
+//Question(0ne)-Declaraction of variable(favoriteNumber)
+const favoriteNumber = 8;
+
+// Question(Two)-
+let halfOfFavNum = favoriteNumber/2;
+// output 4
+
+//Question (Three)- Declaration of variable (isaacsFavorite)
+let isaacsFavorite = 13;
+
+// Question(Four) - Subtraction of favorite number from Isaac’s.
+isaacsFavorite = isaacsFavorite - favoriteNumber;
+//output 5
+
+// Question(Five) - reassigning isaacsFavorite to be 26 times 
+isaacsFavorite = isaacsFavorite * 26;
+//output 130
+
+// Question(six)-
+isaacsFavorite = isaacsFavorite % 3;
+// output 1
